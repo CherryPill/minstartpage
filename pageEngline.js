391406e372e3a7ac24d653f2dd27ddbba6375c80
CherryPill <toshioshitsu@gmail.com>
2019-03-21 20:23:31 +0300

    Initial setup

--- pageEngline.js
+++ pageEngline.js
@@ -1,6 +1,7 @@
 var state = {
 	settingsWindowOpen: false,
-	currentTab: 0
+	currentTab: 0,
+	colorAutoDetectOn: false,
 };
 
 var searchEngines = {
@@ -12,13 +13,13 @@ var searchEngines = {
 };
 //bk and fg
 var webAppColors = {
-	vk: ["#45688E", "color"],
-	fb: ["color", "color"],
-	ig: ["color", "color"],
-	tg: ["color", "color"],
+	vk: ["#45688E", "#FFFFFF"],
+	fb: ["#3C5A99", "#FFFFFF"],
+	ig: ["#E1306C", "#FFFFFF"],
+	tg: ["#0088cc", "#FFFFFF"],
 	sc: ["color", "color"],
 	ggl: ["color", "color"],
-	yh: ["color", "color"],
+	yh: ["#410093", "#FFFFFF"],
 	at: ["color", "color"],
 	mal: ["color", "color"],
 	rdt: ["color", "color"],
@@ -26,7 +27,7 @@ var webAppColors = {
 	yt: ["color", "color"],
 	bd: ["color", "color"],
 	az: ["color", "color"],
-	tw: ["color", "color"]
+	tw: ["color", "color"],
 };
 //twitch, netflix, mail.ru, ph, xv, imdb, ebay, linkedin,
 var webAppShortNames = {
@@ -44,7 +45,7 @@ var webAppShortNames = {
 	yt: "yt",
 	bd: "bd",
 	az: "az",
-	tw: "tw"
+	tw: "tw",
 };
 
 function generateSearchEngineOptions(){
@@ -67,6 +68,8 @@ function initStartPage(){
 	generateSearchEngineOptions();
 	initTimeScript();
 	addTileContextMenuTest();
+	prepareUI(userData);
+	addEventListeners();
 }
 
 function toggleSettingsMenu(){
@@ -101,6 +104,137 @@ function addTileContextMenuTest(){
 	}, false);
 }
 
+//example user data
+var userData = {
+	sections: [
+		{sectionName: "NEWS",
+		sectionItems: [{sectionItemName: "stackoverflow",
+									sectionItemNameShort: "so",
+									sectionItemUrl: "stackoverflow.com",
+								  sectionItemColors: ["#000", "#FF8C00"]},{
+										sectionItemName: "myanimelist",
+																	sectionItemNameShort: "ma",
+																	sectionItemUrl: "myanimelist.net",
+																	sectionItemColors: ["white", "blue"]
+									}]},
+									{sectionName: "WORK",
+									sectionItems: [{sectionItemName: "stackoverflow",
+																sectionItemNameShort: "so",
+																sectionItemUrl: "stackoverflow.com",
+															 sectionItemColors: ["#fff", "#000"] },{
+																	sectionItemName: "tutby",
+																								sectionItemNameShort: "tu",
+																								sectionItemUrl: "tut.by",
+																								sectionItemColors: ["#fff", "#000"]
+																}]},
+																{sectionName: "SOCIAL",
+																sectionItems: [{sectionItemName: "vkontakte",
+																							sectionItemNameShort: "vk",
+																							sectionItemUrl: "vkontakte.com",
+																						sectionItemColors: ["#fff", "#000"] },{
+																								sectionItemName: "facebook",
+																															sectionItemNameShort: "fb",
+																															sectionItemUrl: "facebook.com",
+																															sectionItemColors: ["#fff", "#000"]
+																							}]},
+		],
+};
+
+
+
+function prepareUI(userData){
+	for(section of userData.sections){
+		createSection(section);
+	}
+}
+
+function createSection(sectionItemObj){
+
+	let mainArea = document.getElementById("mainArea");
+	let mainAreaRow = document.createElement("div");
+	mainAreaRow.className = "b";
+	let sectionHeaderRow = document.createElement("div");
+	sectionHeaderRow.className = "sectionHeaderRow";
+	let sectionHeaderName = document.createElement("div");
+	sectionHeaderName.className = "sectionHeaderName";
+	sectionHeaderName.innerHTML = sectionItemObj.sectionName;
+	let sectionHeaderManagement = document.createElement("div");
+	sectionHeaderManagement.className = "sectionHeaderManagement";
+	let sectionHeaderManagementEdit = document.createElement("div");
+	sectionHeaderManagementEdit.className = "sectionHeaderManagementEdit";
+	let sectionHeaderManagementEditAnchor = document.createElement("a");
+	sectionHeaderManagementEditAnchor.innerHTML = "e";
+	let sectionHeaderManagementRem = document.createElement("div");
+	sectionHeaderManagementRem.className = "sectionHeaderManagementRem";
+	let sectionHeaderManagementRemAnchor = document.createElement("a");
+	sectionHeaderManagementRemAnchor.innerHTML = "r";
+
+	let linkTilesSection = document.createElement("div");
+	linkTilesSection.className = "linkTilesSection";
+	for(sectionItem of section.sectionItems){
+		let linkTileAnchor = document.createElement("a");
+		linkTileAnchor.setAttribute("href", sectionItem.sectionItemUrl);
+		linkTileAnchor.setAttribute("target", "_blank");
+		let linkTileAnchorInnerDiv = document.createElement("div");
+		linkTileAnchorInnerDiv.style.color = sectionItem.sectionItemColors[0];
+		linkTileAnchorInnerDiv.style.backgroundColor = sectionItem.sectionItemColors[1];
+		linkTileAnchorInnerDiv.className = "linkTile";
+		linkTileAnchorInnerDiv.innerHTML = sectionItem.sectionItemNameShort;
+		linkTileAnchor.appendChild(linkTileAnchorInnerDiv);
+		linkTilesSection.appendChild(linkTileAnchor);
+	}
+	let linkTileAnchorAddNew = document.createElement("a");
+	linkTileAnchorAddNew.setAttribute("href","#");
+	let linkTileAnchorAddNewInnerDiv = document.createElement("div");
+	linkTileAnchorAddNewInnerDiv.className = "linkTile addNew";
+	linkTileAnchorAddNewInnerDiv.style.color = "black";
+	linkTileAnchorAddNewInnerDiv.style.backgroundColor = "white";
+	linkTileAnchorAddNew.appendChild(linkTileAnchorAddNewInnerDiv);
+	linkTilesSection.appendChild(linkTileAnchorAddNew);
+	sectionHeaderManagementEdit.appendChild(sectionHeaderManagementEditAnchor);
+	sectionHeaderManagementRem.appendChild(sectionHeaderManagementRemAnchor);
+	sectionHeaderManagement.appendChild(sectionHeaderManagementEdit);
+	sectionHeaderManagement.appendChild(sectionHeaderManagementRem);
+	sectionHeaderRow.appendChild(sectionHeaderName);
+	sectionHeaderRow.appendChild(sectionHeaderManagement);
+	mainAreaRow.appendChild(sectionHeaderRow);
+	mainAreaRow.appendChild(linkTilesSection);
+	mainArea.appendChild(mainAreaRow);
+}
+
+function toggleColorLock(_state_){
+	let colorInputs = document.getElementsByClassName("colorPickerInput");
+	let colorInputLabels = document.getElementsByClassName("colorPickerInputLabel");
+	console.log(colorInputLabels);
+	let labelColor, labelBkColor;
+	if(_state_){
+		labelBkColor = "#fff";
+	}
+	else{
+		labelBkColor = "#ccc";
+
+	}
+	for(cil of colorInputLabels){
+		cil.style.backgroundColor = labelBkColor;
+	}
+	for(ci of colorInputs){
+		ci.disabled = !_state_;
+	}
+}
+
+function addEventListeners(){
+	let s = document.getElementById("colorAutoID");
+	s.addEventListener("click", function(){
+				toggleColorLock(state.colorAutoDetectOn)
+		state.colorAutoDetectOn = !state.colorAutoDetectOn;
+	});
+	//let section = document.getElementById()
+}
+
 function generateContextMenu(id){
+
+}
+
+function autoSetColor(url){
 
 }
