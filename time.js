acf5513c199b96d555985ff00a7c0463ae53249c
CherryPill <toshioshitsu@gmail.com>
2019-08-09 10:15:56 +0300

    #15 progress

--- time.js
+++ time.js
@@ -1,6 +1,5 @@
 //gets from config in local storage
-//year: numeric, 2-digit, month: long short, weekday: long short,
-//date_post_fx: true false, _am_pm: true false, seconds true false
+let timeHtmlElement = document.getElementById("clock");
 
 let testTimePattern = "%A, %B %Y | %H:%M:%S"; //current time format
 //supported time format specifiers
@@ -43,7 +42,6 @@ var TimeUtils = {
      * @return {string}
      */
     B: function () {
-        this.currentTime === undefined ? this.currentTime = new Date() : this.currentTime;
         return this.currentTime.toLocaleString("en-us",
             {month: "long"});
     },
@@ -84,14 +82,11 @@ var TimeUtils = {
     },
     //day of the month postfix (th, nd, rd, etc)
     f: function () {
-        timeObj.dateNameNow = this.currentTime.getDate();
-        timeObj.getDayPostFix();
-        return timeObj.dayPostFix;
+        return dayPostFixNew(this.currentTime.getDate());
 
     },
     //Week-based year, last two digits (00-99)
     g: function () {
-        this.currentTime === undefined ? this.currentTime = new Date() : this.currentTime;
         return this.currentTime.getFullYear();
     },
     //	Week-based year
@@ -242,7 +237,7 @@ function parseTimePattern() {
             SavedTimeOptions[testTimePattern[c + 1]] = true;
         }
     console.log(SavedTimeOptions);
-        TimeUtils.currentTime = new Date();
+    TimeUtils.currentTime = new Date();
     for (let setOption in SavedTimeOptions) {
         if (SavedTimeOptions[setOption] === true) {
             finalTimeString = finalTimeString.replace("%" + setOption,
@@ -251,103 +246,6 @@ function parseTimePattern() {
     }
     return finalTimeString;
 }
-
-//%dd means [01-31]+th
-function TimeOptions(_year, _month, _weekday, _date_post_fx, _am_pm, _seconds) {
-    this.year = _year;
-    this.month = _month;
-    this.weekday = _weekday;
-    this.date_post_fx = _date_post_fx;
-    this.am_pm = _am_pm;
-    this.seconds = _seconds;
-}
-
-let timeObj = {
-    htmlClockElement: document.getElementById("clock"),
-    timeNow: null,
-    amPM: null,
-    dateNameNow: null,
-    dayNameNow: null,
-    monthNameNow: null,
-    dayPostFix: null,
-    hrsNow: null,
-    minNow: null,
-    secNow: null,
-    yearNow: null,
-    getTimeStr: function (options) {
-        this.timeNow = new Date();
-        this.yearNow = this.timeNow.toLocaleString("en-us", {year: options.year});
-        this.dayNameNow = this.timeNow.toLocaleString("en-us", {weekday: options.weekday});
-        this.monthNameNow = this.timeNow.toLocaleString("en-us", {month: options.month});
-        this.dateNameNow = this.timeNow.getDate();
-        this.secNow = this.timeNow.getSeconds();
-        this.minNow = this.timeNow.getMinutes();
-        this.hrsNow = this.timeNow.getHours(2);
-        this.minNow = this.digitCorrection(this.minNow);
-        if (options.year === "2-digit") {
-            let t = this.yearNow.split("");
-            t.unshift("'");
-            this.yearNow = t.join("");
-        }
-        if (options.date_post_fx)
-            this.getDayPostFix();
-        if (options.seconds)
-            this.secNow = this.digitCorrection(this.secNow);
-        if (options.am_pm)
-            this.resolveAmPm();
-        return this.formString();
-    },
-    formString: function () {
-        return this.dayNameNow + ", " + this.monthNameNow + " " +
-            this.dateNameNow + this.dayPostFix + ", " +
-            this.yearNow + " | " +
-            this.hrsNow + ":" + this.minNow + ":" + this.secNow + this.amPM + "<br />";
-    },
-    getDayPostFix: function () {
-        if (this.dateNameNow < 4) {
-            switch (this.dateNameNow) {
-                case 1:
-                    this.dayPostFix = "st";
-                    break;
-                case 2:
-                    this.dayPostFix = "nd";
-                    break;
-                default:
-                    this.dayPostFix = "rd";
-                    break;
-            }
-        } else if (this.dateNameNow > 20) {
-            if (!(this.dateNameNow % 11))
-                this.dayPostFix = "nd";
-            else if (this.dateNameNow === 23 || this.dateNameNow === 33)
-                this.dayPostFix = "rd";
-            else if (this.dateNameNow === 21 || this.dateNameNow === 31)
-                this.dayPostFix = "st";
-            else
-                this.dayPostFix = "th";
-        } else {
-            this.dayPostFix = "th";
-        }
-    },
-    digitCorrection: function (c) {
-        if (c < 10) {
-            c = "0" + c;
-        }
-        return c;
-    },
-    resolveAmPm: function (mode) {
-        this.hrsNow = convertTo12HRFormat(this.hrsNow);
-        if (this.hrsNow === 0) {
-            this.amPM = "AM";
-        } else if (this.hrsNow < 12) {
-            this.amPM = "AM";
-        } else if (this.hrsNow === 12) {
-            this.amPM = "PM";
-        } else {
-            this.amPM = "PM";
-        }
-    }
-};
 
 function convertTo12HRFormat(hrs) {
     if (hrs === 0) {
@@ -378,8 +276,37 @@ function digitCorrectionNew(c) {
     return c < 10 ? c = "0" + c : c;
 }
 
+function dayPostFixNew(day) {
+    let dayPostFix = "";
+    if (day < 4) {
+        switch (day) {
+            case 1:
+                dayPostFix = "st";
+                break;
+            case 2:
+                dayPostFix = "nd";
+                break;
+            default:
+                dayPostFix = "rd";
+                break;
+        }
+    } else if (day > 20) {
+        if (!(day % 11))
+            dayPostFix = "nd";
+        else if (day === 23 || day === 33)
+            dayPostFix = "rd";
+        else if (day === 21 || day === 31)
+            dayPostFix = "st";
+        else
+            dayPostFix = "th";
+    } else {
+        dayPostFix = "th";
+    }
+    return dayPostFix;
+}
+
 function initTimeScript() {
-    timeObj.htmlClockElement.innerHTML = "Today is " + parseTimePattern();
+    timeHtmlElement.innerHTML = "Today is " + parseTimePattern();
     let t = setTimeout(function () {
         initTimeScript()
     }, 500);
